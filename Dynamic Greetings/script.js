1c5f3c5432da03bf45178499a0f5fc84c5703503
srikanth-kadiveti <kadivetisrikanth001@gmail.com>
2021-06-12 19:40:38 +0530

    srikanth002

---
+++ Dynamic Greetings/script.js
@@ -0,0 +1,28 @@
+but1=document.querySelector("button");
+op = document.querySelector(".output")
+but1.addEventListener("click",tellGreeting);
+
+function tellGreeting()
+{
+    let dat=new Date();
+    let mes="not assigned";
+    let hr =dat.getHours();
+    
+    if(hr>1||hr<12)
+    {
+    mes="Good Morning!";
+    }
+    if(hr>12||hr<=18)
+    {
+    mes="Good Afternoon!";
+    }
+    if (hr>18||hr<=24)
+    {
+    mes="Good Evening!";
+    }
+    else 
+    mes="Good Night!";
+
+    op.innerHTML=hr+' '+mes;
+
+}
