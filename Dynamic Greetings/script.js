767db2b2ea346b0e8914fc0f83809dcdc6baf972
Abhishek Choudhary <abhishekchoudhary@Abhisheks-MacBook-Pro.local>
2021-06-15 22:41:56 +0530

    upadted js

--- Dynamic Greetings/script.js
+++ Dynamic Greetings/script.js
@@ -1,4 +1,5 @@
 b1 = document.querySelector("button")
+output = document.querySelector(".output")
 
 b1.addEventListener("click", displayMessage);
 
@@ -6,7 +7,17 @@ function displayMessage()
 {
  let date  = new Date()
  let hours = date.getHours()
- if(hours > 17 && hours <=22){
-      console.log("Good Evenning")
+
+ if(hours <12){
+      output.innerHTML="Good Morning"
  }
+ else if(hours >=12 && hours <=17){
+     output.innerHTML="Good Afternoon"
+}
+else if(hours >17 && hours <=22){
+     output.innerHTML="Good evening"
+}
+else{
+     output.innerHTML="Good Night" 
+}
 }
